d120cb5cc6d8caf668f904b9b72ae32709ee45b4
ivanov <ivanov@ascon.ru>
2024-02-19 02:28:48 +0500

    Доработка фронтенда

--- Backend/bin/web/main.js
+++ Backend/bin/web/main.js
@@ -1,21 +1,35 @@
 async function buttonSendClick(){
     let elem = document.querySelector('#task-input')
-    let response = await fetch('/api/task', {
+    await fetch('/api/task', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json;charset=utf-8'
         },
         body: elem.value
       });
-    let result = await response.text()
-    
-    alert(result)
+    buttonResultsClick()
 }
 
 async function buttonResultsClick(){
-  let elem = document.querySelector('#results')
+  let output = document.querySelector('#results');
+  if (!output) return
   let response = await fetch('/api/task');
-  let result = await response.text()
+  let result = await response.json();
+  while (output.firstChild) {
+    output.removeChild(output.firstChild);
+  }
+
+  for (let task of result){
+    let resultText = task.TaskText;
+    if (task.Status == 0) resultText = "[pending] " + resultText;
+    else if (task.Status == 1) resultText = "[done] " + resultText + "=" + task.Result;
+    else if (task.Status == 2) resultText = "[error] " + resultText;
+    li = document.createElement('li')
+    li.innerHTML = resultText
+    output.append(li)
+  }
+
+    
   
-  alert(result)
+  //alert(result)
 }
