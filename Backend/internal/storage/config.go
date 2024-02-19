7d2ab0a6be809bbb3370d8a15d4ca7b166b588e0
ivanov <ivanov@ascon.ru>
2024-02-19 15:57:39 +0500

    правка конфига

--- Backend/internal/storage/config.go
+++ Backend/internal/storage/config.go
@@ -11,5 +11,5 @@ type ConfigStruct struct {
 var Config ConfigStruct
 
 func NewConfig() ConfigStruct {
-	return ConfigStruct{PlusDuration: 2, MinusDuration: 2, MulDuration: 2, DivideDuration: 2, AgentWaitTimeout: 60}
+	return ConfigStruct{PlusDuration: 10, MinusDuration: 10, MulDuration: 10, DivideDuration: 10, AgentWaitTimeout: 60}
 }
